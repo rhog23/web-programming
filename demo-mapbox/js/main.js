9e3c0e05f6a9b3f49168f9cc693f2740837446b3
rhog23 <raymonde.saragih@gmail.com>
2022-11-03 14:13:03 +0700

    update leaflet and mapbox

---
+++ demo-mapbox/js/main.js
@@ -0,0 +1,59 @@
+window.onload = function () {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoibWFwYm94cmVjaXBlcyIsImEiOiJjd3RhQmlzIn0.Wx0fWGCo3gs6fzta5QrLfw";
+  const geojson = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        properties: {
+          color: "#d90429",
+          title: "Maha Vihara Duta Maitreya",
+          description:
+            "42HM+QVG, Komplek Maha Vihara Duta Maitreya Bukit Beruntung, Sungai Panas, Kec. Batam Kota, Kota Batam, Kepulauan Riau 29432",
+        },
+        geometry: {
+          coordinates: [104.03429751928405, 1.1293566340207235],
+          type: "Point",
+        },
+      },
+      {
+        type: "Feature",
+        properties: {
+          color: "#f77f00",
+          title: "Alun-Alun Engku Putri",
+          description:
+            "43H3+CRQ, Jl. Engku Putri, Tlk. Tering, Kec. Batam Kota, Kota Batam, Kepulauan Riau 29444",
+        },
+        geometry: {
+          coordinates: [104.05456841551239, 1.1286593922382622],
+          type: "Point",
+        },
+      },
+    ],
+  };
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [104.03446700110115, 1.1312897724815876],
+    zoom: 12.8,
+    projection: "globe",
+  });
+
+  map.on("style.load", () => {
+    map.setFog({});
+  });
+
+  for (const marker of geojson.features) {
+    new mapboxgl.Marker({
+      color: marker.properties.color,
+    })
+      .setLngLat(marker.geometry.coordinates)
+      .setPopup(
+        new mapboxgl.Popup().setHTML(
+          `<h3>${marker.properties.title}</h3><p><strong>Alamat:</strong> ${marker.properties.description}</p>`
+        )
+      )
+      .addTo(map);
+  }
+};
