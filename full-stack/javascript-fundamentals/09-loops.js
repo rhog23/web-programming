3895709b9bf290d14debadcd8bd2d8aff45b3f1b
rhog23 <raymonderzsaragih@gmail.com>
2023-01-19 15:59:54 +0700

    update looping

--- full-stack/javascript-fundamentals/09-loops.js
+++ full-stack/javascript-fundamentals/09-loops.js
@@ -26,3 +26,43 @@ outer: for (let i = 0; i < 3; i++) {
   }
 }
 alert("Done!");
+
+// Tasks
+for (let i = 2; i <= 10; i += 2) {
+  alert(i);
+}
+
+let i = 0;
+while (i < 3) {
+  alert(`number ${i}!`);
+  i++;
+}
+
+while (true) {
+  let input = prompt("Enter a number:");
+  if (input > 100 || !input) {
+    alert(input);
+    break;
+  }
+}
+
+let num;
+do {
+  num = prompt("Enter a number:");
+} while (num <= 100 && num);
+
+let n = +prompt("Enter a value:", 2);
+prime: for (let i = 2; i <= n; i++) {
+  for (let j = i; j >= 1; j--) {
+    if (i % j == 0 && j != i && j != 1) continue prime;
+  }
+  alert(i);
+}
+
+let n = +prompt("Enter a value:");
+prime: for (let i = 2; i <= n; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue prime;
+  }
+  alert(i);
+}
