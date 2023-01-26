f708067383300e683b0aab3bbd4ef9ae4a812553
rhog23 <raymonderzsaragih@gmail.com>
2023-01-26 17:48:32 +0700

    initial react-fundamentals

---
+++ full-stack/javascript-fundamentals/data-types/01-numbers.js
@@ -0,0 +1,12 @@
+// toString(base)
+let num = 255;
+console.log(num.toString(16));
+console.log(num.toString(2));
+
+// toFixed; returns string
+let num = 12.34;
+console.log(num.toFixed(1));
+
+// parseInt & parseFloat
+console.log(parseInt('100px'));
+console.log(parseFloat('12.5em'));
---
+++ full-stack/react-fundamentals/first-react/demo/index.js
@@ -0,0 +1,117 @@
+/* function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function MainContent() {
+  return <h1>I'm learning React!</h1>;
+}
+ReactDOM.render(
+  <div>
+    <Navbar />
+    <MainContent />
+  </div>,
+  document.getElementById("root")
+); */
+
+/* vanilla JS */
+/* const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way to program";
+h1.className = "header";
+document.getElementById("root").append(h1); */
+
+/* const page = (
+  <div>
+    <h1 className="header">This is JSX</h1>
+    <p>This is a paragraph</p>
+  </div>
+); */
+
+const navbar = (
+  <nav>
+    <h1>Jim's Pet Shop</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+ReactDOM.render(navbar, document.getElementById("root"));
