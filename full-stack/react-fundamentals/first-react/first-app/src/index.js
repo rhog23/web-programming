4866ddee5a974c6d6c3bdc6e6ffab202b9f88c21
rhog23 <raymonderzsaragih@gmail.com>
2023-01-26 21:31:05 +0700

    created fun facts about react page

--- full-stack/react-fundamentals/first-react/first-app/src/index.js
+++ full-stack/react-fundamentals/first-react/first-app/src/index.js
@@ -11,14 +11,15 @@ import "./index.css";
 
 const page = (
   <div>
-    <h1>My awesome website in React</h1>
-    <h3>Reasons I love React</h3>
-    <ol>
-      <li>It's composable</li>
-      <li>It's declarative</li>
-      <li>It's a hireable skill</li>
-      <li>It's actively maintained by skilled people</li>
-    </ol>
+    <img alt="react logo" src="logo512.png" width="40px" height="40px" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
   </div>
 );
 
