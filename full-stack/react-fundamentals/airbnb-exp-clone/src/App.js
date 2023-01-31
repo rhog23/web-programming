bebe686f8f216fe592556a2e9838695f74978bc0
rhog23 <raymonderzsaragih@gmail.com>
2023-01-31 16:10:34 +0700

    updated cards with data

--- full-stack/react-fundamentals/airbnb-exp-clone/src/App.js
+++ full-stack/react-fundamentals/airbnb-exp-clone/src/App.js
@@ -1,38 +1,29 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
 
 export default function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        coverImg={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+        openSpots={item.openSpots}
+        description={item.description}
+      />
+    );
+  });
   return (
     <div className="container">
       <Navbar />
       <Hero />
-      <section className="card--section">
-        <Card
-          img="person-1.png"
-          ratingScore="5.0"
-          ratingCount="6"
-          country="USA"
-          info="Life lessons with Katie Zaferes"
-          price="136"
-        />
-        <Card
-          img="person-2.png"
-          ratingScore="4.5"
-          ratingCount="100"
-          country="Singapore"
-          info="Life lessons with Long Zi Long"
-          price="1120"
-        />
-        <Card
-          img="person-4.png"
-          ratingScore="4.1"
-          ratingCount="1289"
-          country="Malaysia"
-          info="Life lessons with Lee Zi Long"
-          price="15"
-        />
-      </section>
+      <section className="card--section">{cards}</section>
     </div>
   );
 }
--- full-stack/react-fundamentals/airbnb-exp-clone/src/components/Card.js
+++ full-stack/react-fundamentals/airbnb-exp-clone/src/components/Card.js
@@ -1,25 +1,38 @@
 // import profilePic from "../images/person-1.png";
 export default function Card(props) {
-  const { img, ratingScore, ratingCount, country, info, price } = props;
+  const {
+    key,
+    title,
+    description,
+    price,
+    coverImg,
+    rating,
+    reviewCount,
+    location,
+    openSpots,
+  } = props;
   return (
     <div className="card">
+      <p hidden>{key}</p>
       <img
         className="card--img"
-        src={require(`../images/${img}`)}
+        src={require(`../images/${coverImg}`)}
         alt="mentor profile pic"
       />
       <div className="card--rating">
         <i class="la la-star"></i>
-        <span className="card--rating_score">{ratingScore}</span>
-        <span className="card--rating_count gray-text">({ratingCount})</span>
+        <span className="card--rating_score">{rating}</span>
+        <span className="card--rating_count gray-text">({reviewCount})</span>
         <span className="gray-text">•</span>
-        <span className="card--country gray-text">{country}</span>
+        <span className="card--country gray-text">{location}</span>
       </div>
-      <p className="card--info">{info}</p>
+      <p className="card--title">{title}</p>
+      <p>{description}</p>
       <p>
         <strong>From $</strong>
         <span className="card--price">{price}</span> / person
       </p>
+      <p>Available spots: {openSpots}</p>
     </div>
   );
 }
