75f0361a2eb6f1c9ea93706d4611a81c1a824e89
rhog23 <raymonde.saragih@gmail.com>
2023-04-17 17:02:52 +0700

    add api and front page

--- full-stack/javascript-fundamentals/js-rpg/script.js
+++ full-stack/javascript-fundamentals/js-rpg/script.js
@@ -17,8 +17,27 @@ const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
-const monsterNameText = document.querySelector("#monsterNameText");
-const monsterHealthText = document.querySelector("#monsterHealthText");
+const monsterNameText = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+const weapons = [
+  {
+    name: "stick",
+    power: 5,
+  },
+  {
+    name: "dagger",
+    power: 30,
+  },
+  {
+    name: "claw hammer",
+    power: 50,
+  },
+  {
+    name: "sword",
+    power: 100,
+  },
+];
 
 const locations = [
   {
@@ -36,6 +55,12 @@ const locations = [
     ],
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store.",
+  },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters.",
   },
 ];
 
@@ -63,12 +88,23 @@ function goStore() {
 }
 
 function goCave() {
-  console.log("Going to cave");
+  update(locations[2]);
 }
 
 function fightDragon() {
   console.log("Fight dragon");
 }
 
-function buyHealth() {}
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy extra health.";
+  }
+}
 function buyWeapon() {}
+function fightSlime() {}
+function fightBeast() {}
---
+++ full-stack/movie-site/script.js
@@ -0,0 +1,40 @@
+const API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9549acd684db225930b009a5380931cb&page=1";
+const IMAGE_PATH = "https://www.themoviedb.org/t/p/w220_and_h330_face";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=9549acd684db225930b009a5380931cb&query=";
+
+const main = document.getElementById("movie--cards");
+const from = document.getElementById("form");
+const search = document.getElementById("query");
+
+function getMovies(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      data.results.forEach((result) => {
+        const card = `<div class="card">
+        <img
+          class="card--image"
+          src=${IMAGE_PATH + result.poster_path}
+          alt=""
+        />
+        <div class="card--content">
+          <div class="card--title">
+            <h3>${result.title}</h3>
+          </div>
+          <div class="card--body">
+            <p>
+              ${result.overview.substring(0, 50) + "..."}
+            </p>
+          </div>
+        </div>
+      </div>`;
+        main.innerHTML += card;
+      });
+    });
+}
+
+
+
+getMovies(API);
