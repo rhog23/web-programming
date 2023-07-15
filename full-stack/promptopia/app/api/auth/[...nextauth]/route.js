cb7e282b922082b57a20ecd4fec14ce2c7c0390a
rhog23 <raymonde.saragih@gmail.com>
2023-07-15 16:09:41 +0700

    fixed the user database.js and route.js

--- full-stack/promptopia/app/api/auth/[...nextauth]/route.js
+++ full-stack/promptopia/app/api/auth/[...nextauth]/route.js
@@ -10,37 +10,39 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    // get data of user in the running session
-    const sessionUser = await User.findOne({ email: session.user.email });
+  callbacks: {
+    async session({ session }) {
+      // get data of user in the running session
+      const sessionUser = await User.findOne({ email: session.user.email });
 
-    // update the session user id
-    session.user.id = sessionUser._id.toString();
+      // update the session user id
+      session.user.id = sessionUser._id.toString();
 
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      // serverless
-      await connectToDB();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        // serverless
+        await connectToDB();
 
-      // check if the user already exists
-      const userExists = await User.findOne({ email: profile.email });
+        // check if the user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-      //   if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        //   if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
