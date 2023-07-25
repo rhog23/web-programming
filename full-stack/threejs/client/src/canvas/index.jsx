08d9c4d64e0e21642627ad8e40945978ca9566f8
rhog23 <raymonde.saragih@gmail.com>
2023-07-25 15:37:40 +0700

    added files in Canvas folder, and sets up the shirt

--- full-stack/threejs/client/src/canvas/index.jsx
+++ full-stack/threejs/client/src/canvas/index.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+import Shirt from "./Shirt";
 
-const Canvas = () => {
-  return <></>;
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      <CameraRig>
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default CanvasModel;
