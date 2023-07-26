69d23216308e7075af39b50c3a31507d1f3ee946
rhog23 <raymonde.saragih@gmail.com>
2023-07-27 01:24:14 +0700

    done with the ColorPicker

--- full-stack/threejs/client/src/components/ColorPicker.jsx
+++ full-stack/threejs/client/src/components/ColorPicker.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
+import { SketchPicker } from "react-color";
+import { useSnapshot } from "valtio";
+import state from "../store";
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>ColorPicker</div>
-  )
-}
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.shirtColor}
+        disableAlpha
+        onChange={(color) => (state.shirtColor = color.hex)}
+      />
+    </div>
+  );
+};
 
-export default ColorPicker
+export default ColorPicker;
--- full-stack/threejs/client/src/store/index.js
+++ full-stack/threejs/client/src/store/index.js
@@ -3,6 +3,7 @@ import { proxy } from "valtio";
 const state = proxy({
   intro: true, // determine whether we are in the home page or note
   color: "#EFBD48",
+  shirtColor: "#EFBD48",
   isLogoTexture: true, // are we currently displaying the logo texture
   isFullTexture: false,
   logoDecal: "./threejs.png",
