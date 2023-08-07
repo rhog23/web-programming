f747dd9a4f7bcc8e0503a74c149d981de79bab5d
rhog23 <raymonde.saragih@gmail.com>
2023-08-07 21:46:28 +0700

    added rendercards and modify Home

--- full-stack/dalle-clone/client/src/pages/Home.jsx
+++ full-stack/dalle-clone/client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-import { Card, FormField, Loader } from "../components";
+import { FormField, Loader, RenderCards } from "../components";
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
+  const [searchText, setSearchText] = useState("");
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -26,7 +27,21 @@ const Home = () => {
             <Loader />
           </div>
         ) : (
-          <></>
+          <>
+            {searchText && (
+              <h2 className="font-medium text-[#666e75] text-xl mb-3">
+                Showing results for{" "}
+                <span className="text-[#222328]">{searchText}</span>
+              </h2>
+            )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <RenderCards data={[]} title="No search results found" />
+              ) : (
+                <RenderCards data={[]} title="No posts found" />
+              )}
+            </div>
+          </>
         )}
       </div>
     </section>
