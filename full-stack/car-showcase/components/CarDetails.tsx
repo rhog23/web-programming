454d79458a755d81e0d397ce7045cd07d803daf3
rhog23 <raymonde.saragih@gmail.com>
2023-08-25 19:43:55 +0700

    finished CarDetails component

--- full-stack/car-showcase/components/CarDetails.tsx
+++ full-stack/car-showcase/components/CarDetails.tsx
@@ -1,9 +1,131 @@
 "use client";
+import Image from "next/image";
 import { CarDetailsProps } from "@/types";
-import { useState } from "react";
+import { useState, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
-  return <div>CarDetails</div>;
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className={"relative z-10"} onClose={closeModal}>
+          {/* Backdrop */}
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          {/* Dialog */}
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                {/* Dialog Panel */}
+                <Dialog.Panel
+                  className={
+                    "relative w-full max-w-lg max-h-[90vh] overflow-y-auto rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5 p-6"
+                  }
+                >
+                  <button
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                    type="button"
+                    onClick={closeModal}
+                  >
+                    <Image
+                      src={"/close.svg"}
+                      alt="close"
+                      width={20}
+                      height={20}
+                      className="object-contain"
+                    />
+                  </button>
+                  <div className="flex-1 flex flex-col gap-3">
+                    <div className="relative w-full h-40 bg-gradient-to-r from-sky-500 to-indigo-500 bg-cover bg-center rounded-t-lg">
+                      <Image
+                        src={"/hero.png"}
+                        alt="car model"
+                        fill
+                        priority
+                        className="object-contain"
+                      />
+                    </div>
+                    {/* Different POV */}
+                    <div className="flex gap-3">
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={"/hero.png"}
+                          alt="car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={"/hero.png"}
+                          alt="car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={"/hero.png"}
+                          alt="car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  {/* Car Details */}
+                  <div className="flex-1 flex flex-col gap-2">
+                    <h2 className="font-semibold text-xl capitalize">
+                      {car.make} {car.model}
+                    </h2>
+                    <div className="mt-3 flex flex-wrap gap-4">
+                      {Object.entries(car).map(([key, value]) => (
+                        <div
+                          key={key}
+                          className="flex justify-between gap-5 w-full text-right"
+                        >
+                          <h4 className="text-grey capitalize">
+                            {key.split("_").join(" ")}
+                          </h4>
+                          <p
+                            className={`text-black-100 font-semibold ${
+                              key === "drive" ? "uppercase" : "capitalize"
+                            }`}
+                          >
+                            {value}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
 };
 
 export default CarDetails;
