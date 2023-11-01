0247178510114526b47272160aa0712c86a2e376
rhog23 <raymonde.saragih@gmail.com>
2023-11-01 18:23:59 +0700

    ✅ Added lusitana font

---
+++ full-stack/nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { DM_Sans, Lusitana } from "next/font/google";
+
+export const dm_sans = DM_Sans({ display: "swap", subsets: ["latin"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
